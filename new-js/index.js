4ddc5985e030b105ab29f2c56167592fac6ceae3
Tvogmbh <63506404+Tvogmbh@users.noreply.github.com>
2022-08-10 21:51:47 +0800

    Add files via upload

---
+++ new-js/index.js
@@ -0,0 +1,58 @@
+document.getElementById('footer').innerHTML = "Copyright © 2022-2023 <a href='http://pro-ivan.cn/'>pro-ivan.cn</a> Powered by<a href=''> Tvogmbh</a>.";
+document.getElementById('GG').innerHTML = "只提供网站页面显示"
+function imgs() {
+    window.open('new-page1.html', '_self');
+};
+function about() {
+    window.open('new-about.html', '_self');
+};
+function comic() {
+    window.open('new-comic.html', '_self');
+};
+function img_bed() {
+    window.open('new-upload.html', '_self');
+};
+function little_game() {
+    window.open('game/', '_self');
+};
+function home() {
+    window.open('new.html', '_self');
+};
+function img_bed_files() {
+    window.open('http://pro-ivan.cn/upload_pic/?C=M;O=D', '_blank');
+};
+function Kkltsit_hypnosis_saluky() {
+    window.open('http://pro-ivan.cn/mangahtml/manga1.html', '_blank');
+};
+function connect_jp_komota() {
+    window.open('http://pro-ivan.cn/mangahtml/manga2.html', '_blank');
+};
+function connect_cn_komota() {
+    window.open('http://pro-ivan.cn/mangahtml/manga3.html', '_blank');
+};
+function Fischl_saluky() {
+    window.open('http://pro-ivan.cn/mangahtml/manga4.html', '_blank');
+};
+function connect2_komota() {
+    window.open('http://pro-ivan.cn/mangahtml/manga5.html', '_blank');
+};
+function line_1() {
+    //具体咋写，我不知道，域名跳转？
+    alert('无法寻找到服务器');
+};
+function line_2() {
+    //具体咋写，我不知道，域名跳转？
+    alert('无法寻找到服务器');
+};
+function line_3() {
+    //具体咋写，我不知道，域名跳转？
+    alert('无法寻找到服务器');
+};
+function line_4() {
+    //具体咋写，我不知道，域名跳转？
+    alert('无法寻找到服务器');
+};
+function line_5() {
+    //具体咋写，我不知道，域名跳转？
+    alert('无法寻找到服务器');
+};
